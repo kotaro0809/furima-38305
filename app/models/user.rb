d571de90027abc9b6787cd5ba415b55e7d3c8496
kotaro0809 <kotaro.prin@gmail.com>
2022-11-20 20:13:11 +0900

    Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: "半角英数字を混合してね"}
+
+         with_options presence: true do
+          validates :nickname
+          validates :first_name
+          validates :family_name
+          validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "カタカナで入力してね"}
+          validates :family_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "カタカナで入力してね"}
+          validates :birth_day
+         end
+
+
+
+         
+  
+  
 end
