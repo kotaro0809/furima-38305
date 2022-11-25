ccc13358a59f9f18d06c7f4e09501c3442962a70
kotaro0809 <kotaro.prin@gmail.com>
2022-11-25 17:46:38 +0900

    status モデル編集
    アクティブハッシュ

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,9 @@ class Item < ApplicationRecord
   #ActiveHashアソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
   
   #ActiveHashバリデーション
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
