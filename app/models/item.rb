5c42a18f124c0ae089f0126ccaac461013a5171c
kotaro0809 <kotaro.prin@gmail.com>
2022-11-25 18:24:02 +0900

    shipping_feeモデル　編集
    アィティブハッシュ

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,10 +11,12 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :shipping_fee
   belongs_to :prefecture
+  belongs_to :shipping_day
   
   #ActiveHashバリデーション
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"} 
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
