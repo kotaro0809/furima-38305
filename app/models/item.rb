84d3360d9bff0386679b89faab5305c3ae6cae3a
kotaro0809 <kotaro.prin@gmail.com>
2022-11-26 18:35:28 +0900

    バリデーション設定
    price 設定範囲

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,4 +19,23 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank"} 
   validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
+  #バリデーション
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :category_id 
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_day_id
+    validates :user_id
+    validates :image
+   end
+   
+   #priceバリデーション
+   with_options presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 } do
+    validates :price
+   end
+   
 end
