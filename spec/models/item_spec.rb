eea7749e14476a2574a217ad2f53840cc9545a36
kotaro0809 <kotaro.prin@gmail.com>
2022-11-27 00:50:14 +0900

    itemテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item, user_id: user.id)
+  end
+
+  describe "新規出品" do
+    context "新規出品できる時" do
+      it "全ての項目を記入すれば出品できデータベースに保存できる" do
+        expect(@item).to be_valid
+      end
+    end
+    context "新規出品できない時" do
+      it 'item_nameが空の場合' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it 'descriptionが空の場合' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'category_idが空の場合' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'status_idが空の場合' do
+        @item.status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it 'shipping_fee_idが空の場合' do
+        @item.shipping_fee_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+      end
+      it 'prefecture_idが空の場合' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'shipping_day_idが空の場合' do
+        @item.shipping_day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+      it 'priceが空の場合' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'user_idが空の場合' do
+        @item.user_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User can't be blank")
+      end
+      it 'priceが半角数値意外の場合' do
+        @item.price = 'aa１１嗚呼嗚呼嗚呼'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceの値が300より安い場合' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it 'priceの値が9999999より高い場合' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+    end
+
+  end
 end
