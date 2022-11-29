b1bfefe57c7b3c34a113f079dca9867fd6bdc97d
kotaro0809 <kotaro.prin@gmail.com>
2022-11-29 14:50:19 +0900

    itemコントローラー編集
    @item = Item.find(params[:id])
    
    beforeアクションで実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  
+  before_action :set_item, only: [:show, :edit, :update]
  
   def index
     @item = Item.all.order(created_at: :desc)
@@ -20,18 +20,18 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+   
   end
 
   def edit
-    @item = Item.find(params[:id])
+   
     unless @item.user_id == current_user.id
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
+   
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -46,6 +46,7 @@ class ItemsController < ApplicationController
                                  :price, :image).merge(user_id: current_user.id)
   end
 
-  
- 
+  def set_item
+    @item = Item.find(params[:id])
+  end 
 end
