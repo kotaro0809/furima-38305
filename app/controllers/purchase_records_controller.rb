e47748ae220bd3278556ba9203ab0330485a7f74
kotaro0809 <kotaro.prin@gmail.com>
2022-12-04 22:22:42 +0900

    controller編集
    privateメソッド
    before_action :move_to_root, only: [:index]
    
    未ログイン購入rootへ遷移
     before_action :authenticate_user!
    
    その他保存の記述

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,11 +1,34 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :move_to_root, only: [:index]
+
   def index
-    @purchase_records_destination = PurchaseRecordsDestination.new
+    @item = Item.find(params[:item_id]) 
+    @order = PurchaseRecordsDestination.new
   end
 
   def create
-    
+    # binding.pry
+    @order = PurchaseRecordsDestination.new(purchase_records_destination_params)
+      if  @order.valid?
+        @order.save
+        redirect_to root_path
+      else
+        render :index
+      end
   end
 
-  
+  private
+
+  def purchase_records_destination_params
+    params.require(:purchase_records_destination).permit( :post_code, :prefecture_id, :city, :phon_number, :building, :address).merge(user_id: current_user.id, item_id: params[:item_id])  
+  end
+
+  def move_to_root
+    @item = Item.find(params[:item_id])
+    if @item.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
+
 end
