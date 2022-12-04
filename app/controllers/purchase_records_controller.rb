8eb2460e86542527b2c3c072f3a9cf0dc50e6e7e
kotaro0809 <kotaro.prin@gmail.com>
2022-12-04 17:58:16 +0900

    index createアクション定義

---
+++ app/controllers/purchase_records_controller.rb
@@ -0,0 +1,11 @@
+class PurchaseRecordsController < ApplicationController
+  def index
+    @purchase_records_destination = PurchaseRecordsDestination.new
+  end
+
+  def create
+    
+  end
+
+  
+end
