e7060253c7fb8b2b3ca7c8560d1c45913f9879f8
kotaro0809 <kotaro.prin@gmail.com>
2022-12-08 19:58:00 +0900

    purchase_recordsコントローラー編集
    決済　購入済み商品購入不可の処理

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -11,6 +11,7 @@ class PurchaseRecordsController < ApplicationController
     
     @order = PurchaseRecordsDestination.new(purchase_records_destination_params)
       if  @order.valid?
+        pay_item
         @order.save
         redirect_to root_path
       else
@@ -21,14 +22,23 @@ class PurchaseRecordsController < ApplicationController
   private
 
   def purchase_records_destination_params
-    params.require(:purchase_records_destination).permit( :post_code, :prefecture_id, :city, :phon_number, :building, :address).merge(user_id: current_user.id, item_id: params[:item_id])  
+    params.require(:purchase_records_destination).permit( :post_code, :prefecture_id, :city, :phon_number, :building, :address).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])  
   end
 
   def move_to_root
     @item = Item.find(params[:item_id])
-    if @item.user_id == current_user.id
+    if @item.user_id == current_user.id || @item.purchase_record.present?
       redirect_to root_path
     end
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price,                                  # 商品の値段
+        card: purchase_records_destination_params[:token],    # カードトークン
+        currency: 'jpy'                                       # 通貨の種類（日本円）
+      )
   end
 
 end
