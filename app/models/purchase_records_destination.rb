195bc0391f17f6c1d45bb33e1ee4792d508b1236
kotaro0809 <kotaro.prin@gmail.com>
2022-12-12 18:26:01 +0900

    phon_number のバリデーション編集
    10桁でも可能に変更

--- app/models/purchase_records_destination.rb
+++ app/models/purchase_records_destination.rb
@@ -9,7 +9,7 @@ class PurchaseRecordsDestination
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :address
-    validates :phon_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+    validates :phon_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
     validates :token
     
   end
